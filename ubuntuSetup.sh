72b1cdad8d225e79555bb8fcdb54165887ad2490
Harry Allen <leon.allen22@gmail.com>
2018-01-19 08:58:07 -0600

    Condense installs

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -9,16 +9,13 @@
 # Add Repos
 add-repository ppa:numix/ppa
 add-apt-repository ppa:webupd8team/atom
-apt update
 
-# Upgrade & install packages
+# Update & upgrade
+apt update
 apt upgrade -y
-apt install git
-apt install keepassx
-apt install atom
-apt install numix-gtk-theme
-apt install numix-icon-theme
-apt install numix-icon-theme-circle
+
+# Install my packages
+apt install git keepassx atom numix-gtk-theme numix-icon-theme numix-icon-theme-circle
 
 # Install KVM
 apt install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils virt-viewer virt-manager
